2aec91110f2f0f944c8b4b3e9e5389b9970e1273
Huangshuqi <1105374939@qq.com>
2024-09-24 18:34:18 +1000

    Upload KAN-Transformer code for cls

---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+from .build_models import kat_tiny_gelu_patch16_224, kat_tiny_swish_patch16_224, kat_tiny_patch16_224, \
+    kat_small_swish_patch16_224, kat_base_swish_patch16_224, kat_base_patch16_224, \
+    kat_small_patch16_224, kat_base_gelu_patch16_224, kat_small_gelu_patch16_224
